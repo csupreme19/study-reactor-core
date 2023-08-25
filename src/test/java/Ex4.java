437f20bf2ccb283485c5f5ecda9256727b5cc77f
csupreme19 <csupreme19@gmail.com>
2023-08-25 10:03:48 +0900

    플럭스 핸들링(동기식)

--- src/test/java/Ex4.java
+++ src/test/java/Ex4.java
@@ -214,4 +214,25 @@ public class Ex4 {
         myEventProcessor.dataComplete();
     }
 
+    @Test
+    @DisplayName("플럭스 핸들링(동기식)")
+    void fluxHandle() {
+        // Flux.generate와 유사하게 one-by-one emission
+        Flux<String> alphabet = Flux.just(-1, 30, 13, 9, 20)
+                .handle((i, sink) -> {
+                    String letter = alphabet(i);
+                    if (letter != null)
+                        sink.next(letter);
+                });
+
+        alphabet.subscribe(System.out::println);
+    }
+
+    private String alphabet(int letterNumber) {
+        if (letterNumber < 1 || letterNumber > 26) {
+            return null;
+        }
+        int letterIndexAscii = 'A' + letterNumber - 1;
+        return "" + (char) letterIndexAscii;
+    }
 }
